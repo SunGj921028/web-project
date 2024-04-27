6a8be1451310211794604e25f137f6869b754b07
SunGj921028 <111631555+SunGj921028@users.noreply.github.com>
2024-04-28 02:27:35 +0800

    Add files via upload

---
+++ Rotating_Fan/rotate.js
@@ -0,0 +1,17 @@
+let image = document.getElementById("img");
+
+function myfunon() {
+    image.style.animationDuration = 3 + "s";
+}
+function myfunoff() {
+    image.style.animationDuration = 0 + "s";
+}
+function myfun01() {
+    image.style.animationDuration = 1 + "s";
+}
+function myfun02() {
+    image.style.animationDuration = 0.5 + "s";
+}
+function myfun03() {
+    image.style.animationDuration = 0.1 + "s";
+}
